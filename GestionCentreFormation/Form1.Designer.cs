c3ef89c81a3927115326bce5703138afe4df4cdd
Job SAUL <job.saul2@gmail.com>
2018-01-11 10:00:25 +0100

    Ajout data prj

--- GestionCentreFormation/Form1.Designer.cs
+++ GestionCentreFormation/Form1.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LstThemes = new System.Windows.Forms.DataGridView();
+            this.CmdAjouter = new System.Windows.Forms.Button();
+            this.CmdModifier = new System.Windows.Forms.Button();
+            this.CmdSupprimer = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.LstThemes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LstThemes
+            // 
+            this.LstThemes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.LstThemes.Location = new System.Drawing.Point(13, 13);
+            this.LstThemes.Name = "LstThemes";
+            this.LstThemes.Size = new System.Drawing.Size(878, 225);
+            this.LstThemes.TabIndex = 0;
+            this.LstThemes.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.LstThemes_CellContentClick);
+            // 
+            // CmdAjouter
+            // 
+            this.CmdAjouter.Location = new System.Drawing.Point(333, 242);
+            this.CmdAjouter.Name = "CmdAjouter";
+            this.CmdAjouter.Size = new System.Drawing.Size(75, 23);
+            this.CmdAjouter.TabIndex = 1;
+            this.CmdAjouter.Text = "Ajouter";
+            this.CmdAjouter.UseVisualStyleBackColor = true;
+            this.CmdAjouter.Click += new System.EventHandler(this.CmdAjouter_Click);
+            // 
+            // CmdModifier
+            // 
+            this.CmdModifier.Location = new System.Drawing.Point(414, 242);
+            this.CmdModifier.Name = "CmdModifier";
+            this.CmdModifier.Size = new System.Drawing.Size(75, 23);
+            this.CmdModifier.TabIndex = 2;
+            this.CmdModifier.Text = "Modifier";
+            this.CmdModifier.UseVisualStyleBackColor = true;
+            this.CmdModifier.Click += new System.EventHandler(this.CmdModifier_Click);
+            // 
+            // CmdSupprimer
+            // 
+            this.CmdSupprimer.Location = new System.Drawing.Point(495, 242);
+            this.CmdSupprimer.Name = "CmdSupprimer";
+            this.CmdSupprimer.Size = new System.Drawing.Size(75, 23);
+            this.CmdSupprimer.TabIndex = 3;
+            this.CmdSupprimer.Text = "Supprimer";
+            this.CmdSupprimer.UseVisualStyleBackColor = true;
+            this.CmdSupprimer.Click += new System.EventHandler(this.CmdSupprimer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 277);
+            this.Controls.Add(this.CmdSupprimer);
+            this.Controls.Add(this.CmdModifier);
+            this.Controls.Add(this.CmdAjouter);
+            this.Controls.Add(this.LstThemes);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.LstThemes)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView LstThemes;
+        private System.Windows.Forms.Button CmdAjouter;
+        private System.Windows.Forms.Button CmdModifier;
+        private System.Windows.Forms.Button CmdSupprimer;
     }
 }
 
--- GestionCentreFormation/Form1.cs
+++ GestionCentreFormation/Form1.cs
@@ -12,9 +12,35 @@ namespace GestionCentreFormation
 {
     public partial class Form1 : Form
     {
+        private FormationEntities ContexteDonnees;
+
         public Form1()
         {
             InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ContexteDonnees = new FormationEntities();
+        }
+
+        private void LstThemes_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void CmdAjouter_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CmdModifier_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CmdSupprimer_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
